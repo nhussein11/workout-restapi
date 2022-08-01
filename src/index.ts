031cef6263daa7461b4e8bcad93b177b288bba17
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-01 12:11:34 -0300

    add: apicache middleware to getAllWorkouts route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
 const app = express()
 
 app.use(express.json())
+
 const PORT = process.env.PORT || 3000;
 
 app.use( '/api/v1/workouts', v1WorkoutRouter)
--- src/v1/routes/workoutRoutes.ts
+++ src/v1/routes/workoutRoutes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 
-const workoutController = require('../../controllers/workoutController')
-const recordController = require('../../controllers/recordController')
+const apicache = require('apicache');
+
+const workoutController = require('../../controllers/workoutController');
+const recordController = require('../../controllers/recordController');
 
 const router = express.Router();
 
+const cache = apicache.middleware;
+
 router
-    .get('/', workoutController.getAllWorkouts)
+    .get('/',cache("2 minutes"), workoutController.getAllWorkouts)
     .get('/:workoutId', workoutController.getOneWorkout )
     .get('/:workoutId/records', recordController.getRecordForWorkout)
     .post('/', workoutController.createNewWorkout)
